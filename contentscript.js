2fa4775b5c43b312f1aed6c8c587964b3469ed5c
Tariq Chaudhry <tariqc80@gmail.com>
2016-06-07 08:31:11 -0400

    Fixed bug that added mutlple buttons

--- contentscript.js
+++ contentscript.js
@@ -5,6 +5,7 @@ chrome.storage.sync.get('github2jira', function(settings) {
 });
     
 var init = function(settings) {
+
 	var observer = new MutationObserver(function(mutations) {
 		var branch_containers = getBranchElementsFromPullRequestPage();
 	    if (branch_containers.length > 0) {
@@ -13,8 +14,8 @@ var init = function(settings) {
 	});
 
 	// configuration of the observer:
-	var body = document.getElementsByTagName('body');
-	var config = { attributes: true, childList: true, characterData: true };
+	var body   = document.getElementsByTagName('body');
+	var config = { childList: true, subtree: true };
 	 
 	// pass in the target node, as well as the observer options
 	observer.observe(body[0], config);
@@ -44,26 +45,30 @@ var addGotoJiraButton = function(branch_containers, jira_url) {
 		jira_redirect,
 		button;
 
-	outloop:
-	for (var b_index = 0; b_index < branch_containers.length; b_index++) {
-		branch_names = branch_containers[b_index].getElementsByTagName('span');
-		for (var i = 0; i < branch_names.length; i++) {
-			// find the first branch with the jira ticket name
-			// todo make both to and from branches have links
-				if (branch_parts = branch_names[i].textContent.match(/^(.*)\/(.*-[0-9]+).*$/)) {
-				branch_name_element = branch_names[i];
-				break outloop;
+	var existing_button = document.getElementsByClassName('github-goto-jira');
+	
+	if (existing_button.length < 1) {
+		outloop:
+		for (var b_index = 0; b_index < branch_containers.length; b_index++) {
+			branch_names = branch_containers[b_index].getElementsByTagName('span');
+			for (var i = 0; i < branch_names.length; i++) {
+				// find the first branch with the jira ticket name
+				// todo make both to and from branches have links
+					if (branch_parts = branch_names[i].textContent.match(/^(.*)\/(.*-[0-9]+).*$/)) {
+					branch_name_element = branch_names[i];
+					break outloop;
+				}
 			}
 		}
+
+		jira_redirect = jira_url + '/browse/' + branch_parts[2];
+
+		button = document.createElement('a');
+		button.textContent = 'Goto Jira Ticket';
+		button.setAttribute('href', jira_redirect);
+		button.setAttribute('title', jira_redirect);
+		button.setAttribute('class', 'github-goto-jira');
+
+		branch_name_element.parentNode.nextSibling.parentNode.insertBefore(button, branch_name_element.parentNode.nextSibling);
 	}
-
-	jira_redirect = jira_url + '/browse/' + branch_parts[2];
-
-	button = document.createElement('a');
-	button.textContent = 'Goto Jira Ticket';
-	button.setAttribute('href', jira_redirect);
-	button.setAttribute('title', jira_redirect);
-	button.setAttribute('class', 'github-goto-jira');
-
-	branch_name_element.parentNode.nextSibling.parentNode.insertBefore(button, branch_name_element.parentNode.nextSibling);
 };
